5a1a32274095f31f3869fad13dc78ba8bdfe7bf0
Alexandr Daultukaev <shyra31102000@mail.ru>
2021-07-17 16:02:53 +0700

    Add game_data

---
+++ game_data.py
@@ -0,0 +1,122 @@
+data = [
+    {
+        "name": "Davud Beckham",
+        "followers": 14.5,
+        "description": "Footballer",
+        "country": "London, England"
+    },
+    {
+        "name": "Zendaya",
+        "followers": 14.8,
+        "description": "Actress",
+        "country": "Oakland, California"
+    },
+    {
+        "name": "LeBron James",
+        "followers": 14.9,
+        "description": "Basketball player",
+        "country": "Akron, Ohio"
+    },
+    {
+        "name": "Drake",
+        "followers": 15.5,
+        "description": "Rapper, Singer",
+        "country": "Toronto, Ontario"
+    },
+    {
+        "name": "James Rodríguez",
+        "followers": 15.7,
+        "description": "Footballer",
+        "country": "Cúcuta, Colombia"
+    },
+    {
+        "name": "Justin Timberlake",
+        "followers": 17.1,
+        "description": "Singer, Songwriter, Actor",
+        "country": "Memphis, Tennessee, U.S."
+    },
+    {
+        "name": "Chris Brown",
+        "followers": 17.6,
+        "description": "Singer, Actor",
+        "country": "Tappahannock, Virginia, U.S."
+    },
+    {
+        "name": "Ellen DeGeneres",
+        "followers": 18.8,
+        "description": "Comedian, Television host, Actress",
+        "country": "Metairie, Louisiana, U.S."
+    }, 
+    {
+        "name": "Cara Delevingne",
+        "followers": 21.9,
+        "description": "Model, Actress",
+        "country": "Hammersmith, London, England"
+    }, 
+    {
+        "name": "Kevin Hart",
+        "followers": 24.6,
+        "description": "Actor, Comedian",
+        "country": "Philadelphia, Pennsylvania, U.S."
+    },
+    {
+        "name": "Demi Lovato",
+        "followers": 26.4,
+        "description": "Singer, Songwriter",
+        "country": "Albuquerque, New Mexico, U.S."
+    },
+    {
+        "name": "Lionel Messi",
+        "followers": 27.7,
+        "description": "Footballer",
+        "country": "Rosario, Argentina"
+    },
+    {
+        "name": "Rihanna",
+        "followers": 28.8,
+        "description": "Singer, Actress, Fashion designer",
+        "country": "Saint Michael, Barbados"
+    },
+    {
+        "name": "Jennifer Lopez",
+        "followers": 29.9,
+        "description": "Singer, Actress",
+        "country": "New York City, U.S."
+    },
+    {
+        "name": "Miley Cyrus",
+        "followers": 31,
+        "description": "Singer, Actress",
+        "country": "Franklin, Tennessee, U.S."
+    },
+    {
+        "name": "Katy Perry",
+        "followers": 31.2,
+        "description": "Singer",
+        "country": "Santa Barbara, California, U.S."
+    },
+    {
+        "name": "Dwayne Johnson",
+        "followers": 31.8,
+        "description": "Actor, Producer, Businessman, Retired Professional Wrestler",
+        "country": "Hayward, California, U.S."
+    },
+    {
+        "name": "Neymar",
+        "followers": 34.4,
+        "description": "Footballer",
+        "country": "Mogi das Cruzes, Brazil"
+    },
+    {
+        "name": "Cristiano Ronaldo",
+        "followers": 35.5,
+        "description": "Footballer",
+        "country": "Funchal, Madeira, Portugal"
+    },
+    {
+        "name": "Cristiano Ronaldo",
+        "followers": 35.5,
+        "description": "Footballer",
+        "country": "Funchal, Madeira, Portugal"
+    },
+]
