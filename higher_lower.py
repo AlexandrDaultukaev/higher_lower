a29cd77d7bbc1f0018a370f002b7469109ad5cb3
Alexandr Daultukaev <shyra31102000@mail.ru>
2021-07-17 17:06:03 +0700

    Take out print in print_data func

--- higher_lower.py
+++ higher_lower.py
@@ -1,4 +1,5 @@
 from game_data import data
+import art
 import random
 import os
 
@@ -6,28 +7,32 @@ clear = lambda: os.system('cls')
 
 def getrand(first_number):
     second_number = random.randint(0, 19)
-    if second_number == first_number:
-        second_number -= 1
+    while second_number == first_number:
+        second_number = random.randint(0, 19)
     return second_number
 
+def print_data(first_person, second_person):
+    print("Compare A: " + data[first_person]["name"] + ", a " + data[first_person]["description"] + ", from " + data[first_person]["country"] + ".")
+    print(art.vs)
+    print("Against B: " + data[second_person]["name"] + ", a " + data[second_person]["description"] + ", from " + data[second_person]["country"] + ".\n")
+    choice = input("Who has more followers? Type 'A' or 'B': ").upper()
+    clear()
+    print("A: " + data[first_person]["name"] + " has " + str(data[first_person]["followers"]) + " million followers\n")
+    print("B: " + data[second_person]["name"] + " has " + str(data[second_person]["followers"]) + " million followers\n")
+    return choice
+
 def game():
+    print(art.logo)
     counter = 0
     GAME_OVER = False
     first = getrand(-1)
     second = getrand(first)
-    if first == second:
-        first -= 1
     while not GAME_OVER:
-        print("Compare A: " + data[first]["name"] + ", a " + data[first]["description"] + ", from " + data[first]["country"] + ".\n")
-        print("Against B: " + data[second]["name"] + ", a " + data[second]["description"] + ", from " + data[second]["country"] + ".\n")
-        choice = input("Who has more followers? Type 'A' or 'B': ").upper()
-        clear()
-        print("A: " + data[first]["name"] + " has " + str(data[first]["followers"]) + " million followers\n")
-        print("B: " + data[second]["name"] + " has " + str(data[second]["followers"]) + " million followers\n")
+        first = second
+        second = getrand(first)
+        choice = print_data(first, second)
         if choice == 'A':
             if data[first]["followers"] > data[second]["followers"]:
-                first = second
-                second = getrand(first)
                 counter += 1
                 print("\n\t\t✔✔✔\n\t\tcounter: " + str(counter))
             else:
@@ -35,8 +40,6 @@ def game():
                 print("\n\t\t✖✖✖\n\t\tcounter: " + str(counter))
         elif choice == 'B':
             if data[first]["followers"] < data[second]["followers"]:
-                first = second
-                second = getrand(first)
                 counter += 1
                 print("\n\t\t✔✔✔\n\t\tcounter: " + str(counter))
             else:
